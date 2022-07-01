6257274a2fb3f9c8d9efb25a45c6872756ff6dbc
Ismael Celis <ismaelct@gmail.com>
2022-07-01 13:33:14 +0100

    custom heading anchors

---
+++ static/js/custom.js
@@ -0,0 +1,14 @@
+function addAnchor (heading) {
+  var anchor = document.createElement("a")
+  heading.classList.add("has-anchor")
+  anchor.classList.add("heading-anchor")
+  anchor.setAttribute("href", "#" + heading.id)
+  heading.prepend(anchor)
+}
+
+function addAnchors (evt) {
+  var headings = document.querySelectorAll("h1[id], h2[id], h3[id]")
+  headings.forEach(addAnchor)
+}
+
+window.addEventListener("load", addAnchors)
